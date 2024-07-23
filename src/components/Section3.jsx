c167a0cf037066d99738566412951576b2da958f
Julieta <jbrussa01@gmail.com>
2024-07-23 20:03:10 -0300

    section 3 terminada

--- src/components/Section3.jsx
+++ src/components/Section3.jsx
@@ -5,37 +5,106 @@ import {
   TextoTitulo,
   Filtro,
   Personaje,
-  BordesYLineas,
+  Linea,
+  ContenedorZona,
+  Imagen,
 } from "./styles/Section3.js";
 import { useState } from "react";
 
 export const Section3 = () => {
   // Estado para controlar la visibilidad del tooltip
-  const [isVisible, setIsVisible] = useState(false);
-
-  // Funciones para mostrar/ocultar el tooltip
-  const handleMouseEnter = () => setIsVisible(true);
-  const handleMouseLeave = () => setIsVisible(false);
+  const [isVisibleMondstadt, setIsVisibleMondstadt] = useState(false);
+  const [isVisibleLiyue, setIsVisibleLiyue] = useState(false);
+  const [isVisibleInazuma, setIsVisibleInazuma] = useState(false);
+  const [isVisibleSumeru, setIsVisibleSumeru] = useState(false);
+  const [isVisibleFontaine, setIsVisibleFontaine] = useState(false);
 
   return (
     <>
-      <Zona
-        bgImg="/img/section3/MondstadtRecorte.jpg"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}
-      >
-        <Filtro></Filtro>
-        <TextoTitulo>Mondstadt</TextoTitulo>
-        {isVisible && (
-          <>
-            <Personaje
-              bgImg="/img/section3/personajeMondstadt.png"
-              isVisible={isVisible}
-            ></Personaje>
-            <BordesYLineas>a</BordesYLineas>
-          </>
-        )}
-      </Zona>
+      {/* Mondstadt */}
+      <ContenedorZona>
+        <Zona
+          onMouseEnter={() => setIsVisibleMondstadt(true)}
+          onMouseLeave={() => setIsVisibleMondstadt(false)}
+        >
+          <Imagen bgImg="/img/section3/MondstadtRecorte.jpg"></Imagen>
+          <Filtro></Filtro>
+          <TextoTitulo>Mondstadt</TextoTitulo>
+          <Personaje
+            bgImg="/img/section3/personajeMondstadt.png"
+            isVisible={isVisibleMondstadt}
+          ></Personaje>
+          <Linea isVisible={isVisibleMondstadt}></Linea>
+        </Zona>
+      </ContenedorZona>
+
+      {/* Liyue */}
+      <ContenedorZona>
+        <Zona
+          onMouseEnter={() => setIsVisibleLiyue(true)}
+          onMouseLeave={() => setIsVisibleLiyue(false)}
+        >
+          <Imagen bgImg="/img/section3/LiyueRecorte.jpg"></Imagen>
+          <Filtro></Filtro>
+          <TextoTitulo>Liyue</TextoTitulo>
+          <Personaje
+            bgImg="/img/section3/personajeLiyue.png"
+            isVisible={isVisibleLiyue}
+          ></Personaje>
+          <Linea isVisible={isVisibleLiyue}></Linea>
+        </Zona>
+      </ContenedorZona>
+
+      {/* Inazuma */}
+      <ContenedorZona>
+        <Zona
+          onMouseEnter={() => setIsVisibleInazuma(true)}
+          onMouseLeave={() => setIsVisibleInazuma(false)}
+        >
+          <Imagen bgImg="/img/section3/InazumaRecorte.jpg"></Imagen>
+          <Filtro></Filtro>
+          <TextoTitulo>Inazuma</TextoTitulo>
+          <Personaje
+            bgImg="/img/section3/personajeInazuma.png"
+            isVisible={isVisibleInazuma}
+          ></Personaje>
+          <Linea isVisible={isVisibleInazuma}></Linea>
+        </Zona>
+      </ContenedorZona>
+
+      {/* Sumeru */}
+      <ContenedorZona>
+        <Zona
+          onMouseEnter={() => setIsVisibleSumeru(true)}
+          onMouseLeave={() => setIsVisibleSumeru(false)}
+        >
+          <Imagen bgImg="/img/section3/SumeruRecorte.jpg"></Imagen>
+          <Filtro></Filtro>
+          <TextoTitulo>Sumeru</TextoTitulo>
+          <Personaje
+            bgImg="/img/section3/personajeSumeru.png"
+            isVisible={isVisibleSumeru}
+          ></Personaje>
+          <Linea isVisible={isVisibleSumeru}></Linea>
+        </Zona>
+      </ContenedorZona>
+
+      {/* Fontaine */}
+      <ContenedorZona>
+        <Zona
+          onMouseEnter={() => setIsVisibleFontaine(true)}
+          onMouseLeave={() => setIsVisibleFontaine(false)}
+        >
+          <Imagen bgImg="/img/section3/FontaineRecorte.png"></Imagen>
+          <Filtro></Filtro>
+          <TextoTitulo>Fontaine</TextoTitulo>
+          <Personaje
+            bgImg="/img/section3/personajeFontaine.png"
+            isVisible={isVisibleFontaine}
+          ></Personaje>
+          <Linea isVisible={isVisibleFontaine}></Linea>
+        </Zona>
+      </ContenedorZona>
     </>
   );
 };
--- src/components/styles/Section3.js
+++ src/components/styles/Section3.js
@@ -1,17 +1,60 @@
 import styled from "@emotion/styled";
-import { keyframes } from "@emotion/react";
+import { keyframes, css } from "@emotion/react";
 
-export const Zona = styled.div`
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+export const Imagen = styled.img`
+  width: 100%;
+  height: 100%;
+  transition: transform 0.5s ease; /* Suaviza la transición del zoom */
   background-image: url(${({ bgImg }) => bgImg});
   background-size: cover /* Hace que la imagen cubra todo el contenedor */;
   background-position: center /* Centra la imagen */;
+  position: absolute;
+  overflow: hidden;
+  &:hover {
+    transform: scale(1.1); /* Aplica el zoom */
+  }
+`;
+
+export const ContenedorZona = styled.div`
   height: 28vh;
   width: 99vw;
+  overflow: hidden;
+  & :hover img {
+    transform: scale(1.1);
+  }
+`;
+
+export const Zona = styled.div`
+  height: 100%;
+  width: 100%;
   position: relative; /* Permite agregar contenido encima de la imagen */
   display: flex;
   align-items: center;
   justify-content: center;
   z-index: 0;
+  transition: transform 0.5s ease;
+  & :hover {
+    animation: ${fadeIn} 300ms forwards;
+    opacity: 1;
+  }
 `;
 
 export const Filtro = styled.div`
@@ -34,47 +77,56 @@ export const Estrellas = styled.div`
 `;
 
 export const TextoTitulo = styled.h1`
-  font-size: 2rem;
+  font-size: 2.25rem;
   text-align: center;
   color: white;
   font-weight: 400;
   z-index: 3;
   position: absolute;
-`;
-
-const fadeIn = keyframes`
-  from {
-    opacity: 0;
-  }
-  to {
-    opacity: 1;
-  }
-`;
-
-const fadeOut = keyframes`
-  from {
-    opacity: 1;
-  }
-  to {
-    opacity: 0;
-  }
+  font-family: Tahoma, Helvetica, Arial, sans-serif;
 `;
 
 export const Personaje = styled.div`
-  background-image: url(${({ bgImg }) => bgImg});
   width: 100%;
   height: 100%;
-  background-position: right /* Centra la imagen */;
+  background-position: right;
   z-index: 4;
   background-repeat: no-repeat;
-  animation: ${({ isVisible }) => (isVisible ? fadeIn : fadeOut)} 1000ms
-    forwards;
-  border: 0.2rem solid white;
+  background-image: url(${({ bgImg }) => bgImg});
+  box-shadow: inset 0 0 0 0.3rem white; /* Crea el borde blanco hacia adentro */
+  opacity: 0;
+  ${({ isVisible }) =>
+    isVisible
+      ? css`
+          animation: ${fadeIn} 400ms forwards;
+        `
+      : css`
+          animation: ${fadeOut} 400ms forwards;
+        `};
 `;
 
-export const BordesYLineas = styled.div`
+export const Linea = styled.div`
   position: absolute;
-  width: 100%;
-  height: 100%;
-  z-index: 5;
+  width: 9%;
+  height: 0.8%;
+  align-self: center;
+  justify-self: center;
+  margin-top: 3rem;
+  background: linear-gradient(
+    to right,
+    transparent,
+    #fbfdfe 45% 55%,
+    transparent
+  );
+  z-index: 4;
+  opacity: 0;
+  overflow: hidden;
+  ${({ isVisible }) =>
+    isVisible
+      ? css`
+          animation: ${fadeIn} 400ms forwards;
+        `
+      : css`
+          animation: ${fadeOut} 400ms forwards;
+        `};
 `;
